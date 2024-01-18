065bda33dc4febae77e7c0ffe6c3a33dd2a38f46
Oren Leiman <oren.leiman@redpanda.com>
2024-01-17 16:50:58 -0800

    logger with log_manager
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/transform/logger.h
+++ src/v/transform/logger.h
@@ -12,11 +12,35 @@
 #pragma once
 
 #include "base/seastarx.h"
+#include "transform/log_manager.h"
+#include "wasm/logger.h"
 
 #include <seastar/util/log.hh>
 
 namespace transform {
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-non-const-global-variables)
 extern ss::logger tlog;
+
+class logger final : public wasm::logger {
+    using meta = wasm::logger::meta;
+
+public:
+    logger() = delete;
+    explicit logger(ss::sstring name, ss::logger* lg, log_manager* mgr)
+      : wasm::logger(std::move(name), lg)
+      , _mgr(mgr) {}
+    // TODO(oren): destructor, rule of 5
+    // TODO(oren): assert mgr non-null
+
+    void log(ss::log_level lvl, std::string_view message) noexcept override {
+        get_logger().log(lvl, message);
+        _mgr->enqueue_log(
+          meta{.level = lvl, .transform_name = name()}, message);
+    }
+
+private:
+    // TODO(oren): this should maybe be a shard_ptr
+    log_manager* _mgr{nullptr};
+};
 
 } // namespace transform

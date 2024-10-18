2ea2a4ed5f3fad3f87b1d1b8b0604b4bdabe4b16
Oren Leiman <oren.leiman@redpanda.com>
2024-10-18 13:14:42 -0700

    Better tests, I think this might be basically ok

--- tests/rptest/tests/cloud_storage_props_warn_test.py
+++ tests/rptest/tests/cloud_storage_props_warn_test.py
@@ -16,20 +16,23 @@ from rptest.clients.types import TopicSpec
 from rptest.clients.rpk import RpkTool
 from rptest.services.redpanda import SISettings
 from rptest.tests.redpanda_test import RedpandaTest
-
-# from rptest.tests.prealloc_nodes import PreallocNodesTest
-# from rptest.utils.mode_checks import skip_debug_mode
+from rptest.util import wait_until_result
 
 from ducktape.cluster.cluster import ClusterNode
 from ducktape.mark import matrix
 from ducktape.utils.util import wait_until
 
 
-# PreallocNodesTest
 class CloudStoragePropsWarnTest(RedpandaTest):
+    SNAPSHOT_MAX_AGE_S = 5
+
     def __init__(self, test_context):
         super().__init__(test_context,
                          num_brokers=3,
+                         extra_rp_conf={
+                             'controller_snapshot_max_age_sec':
+                             self.SNAPSHOT_MAX_AGE_S
+                         },
                          si_settings=SISettings(
                              test_context=test_context,
                              cloud_storage_enable_remote_read=False,
@@ -45,23 +48,48 @@ class CloudStoragePropsWarnTest(RedpandaTest):
     def scrape_warning(self, nodes: list[ClusterNode]) -> list[str]:
         matches: list[str] = []
         WARNING_LOG = "'Cloud storage not fully enabled for topic'"
-        for n in self.redpanda.nodes:
+        for n in nodes:
             matches += self.log_searcher._capture_log(n, WARNING_LOG)
         # basically sort by timestamp rather than by node of origin
         return sorted(matches)
 
+    def wait_for_logs(self, expected: int, prev_n: int,
+                      nodes: list[ClusterNode]) -> int:
+        def _check():
+            logs = self.scrape_warning(nodes)
+            diff_n = len(logs) - prev_n
+            new_warnings = logs[-diff_n:] if diff_n > 0 else []
+            self.logger.warn(
+                f"NEW WARNINGS (expect {expected}): {json.dumps(new_warnings, indent=1)}"
+            )
+            assert diff_n <= expected, f"Too many new warnings! Expected {diff_n} got {len(new_warnings)}"
+            return diff_n == expected, logs
+
+        return len(
+            wait_until_result(lambda: _check(),
+                              timeout_sec=10,
+                              backoff_sec=1,
+                              err_msg="Didn't get the right number"))
+
+    def get_controller_leader(self) -> ClusterNode:
+        id = self.admin.await_stable_leader('controller',
+                                            partition=0,
+                                            namespace="redpanda",
+                                            timeout_s=30)
+        return self.redpanda.get_node(id)
+
     @cluster(num_nodes=3)
     @matrix(
         rr=[
-            True,
+            # True,
             False,
         ],
         rw=[
-            True,
+            # True,
             False,
         ],
         rd=[
-            True,
+            # True,
             False,
         ],
     )
@@ -73,31 +101,28 @@ class CloudStoragePropsWarnTest(RedpandaTest):
         }
 
         full = all([rr, rw])
-        disabled = not any([rr, rw])
 
         self._rpk.create_topic('foo', partitions=1, config=topic_props)
 
-        # Scrape logs for the topic config warning and assert that there
-        # are some new occurrences (or not)
-        def _check_logs(expect_warning: bool, prev_n: int = 0):
-            logs = self.scrape_warning(self.redpanda.nodes)
-            print(f"LOOGS: {len(logs)} {json.dumps(logs, indent=1)}")
-            total_n = len(logs)
-            diff_n = total_n - prev_n
-            if expect_warning:
-                assert diff_n > 0, f"Expected new warning, got none"
-            else:
-                assert diff_n == 0, f"Unexpected {diff_n} warnings: {json.dumps(logs[-diff_n:], indent=1)}"
-            return total_n
+        leader = self.get_controller_leader()
 
-        n_logs = _check_logs(not full)
+        n_logs = self.wait_for_logs(1 if not full else 0,
+                                    prev_n=0,
+                                    nodes=[leader])
 
         self.logger.debug(
             "Confirm that the warning is written out again after a restart.")
         self.redpanda.rolling_restart_nodes(self.redpanda.nodes)
-        n_logs = _check_logs(not full, prev_n=n_logs)
+        leader = self.get_controller_leader()
+
+        n_logs = self.wait_for_logs(1 if not full else 0,
+                                    prev_n=n_logs,
+                                    nodes=[leader])
 
         if full:
+            self.logger.debug(
+                "Full cloud storage already enabled, so we're done. Remaining cases check fixing things up."
+            )
             return
 
         self.logger.debug(
@@ -107,18 +132,29 @@ class CloudStoragePropsWarnTest(RedpandaTest):
             self._rpk.alter_topic_config('foo', 'redpanda.remote.write',
                                          'true')
 
-        n_logs = _check_logs(disabled, n_logs)
+        n_logs = self.wait_for_logs(1 if not rr else 0, n_logs, nodes=[leader])
 
         if not rr:
             self._rpk.alter_topic_config('foo', 'redpanda.remote.read', 'true')
 
         self.logger.debug(
-            "Cloud storage should be fully enabled now, the final alter-config producing no warnings"
+            "Cloud storage should be fully enabled now, the final alter-config producing no new warnings"
         )
-        n_logs = _check_logs(False, prev_n=n_logs)
+        n_logs = self.wait_for_logs(0, prev_n=n_logs, nodes=[leader])
 
-        # self.logger.debug(
-        #     "Restart the cluster again. We shouldn't see any new warnings.")
+        for n in self.redpanda.nodes:
+            self.redpanda.wait_for_controller_snapshot(n)
 
-        # self.redpanda.rolling_restart_nodes(self.redpanda.nodes)
-        # n_logs = _check_logs(False, prev_n=n_logs)
+        # for n in self.redpanda.nodes:
+        #     self.redpanda.wait_for_controller_snapshot(n)
+
+        self.logger.debug(
+            "Restart the cluster again. Original CREATE warning should be snapshotted away."
+        )
+
+        self.redpanda.rolling_restart_nodes(self.redpanda.nodes)
+        # TODO(oren): maybe we should get the topic leader?
+        leader = self.get_controller_leader()
+
+        # TODO(oren): we should be able to snapshot far enough that this is 0 eventually, no?
+        n_logs = self.wait_for_logs(1, prev_n=n_logs, nodes=[leader])

f53e715ff62b33dab50039cefabccc88c0271a5a
Oren Leiman <oren.leiman@redpanda.com>
2025-09-21 21:57:30 -0700

    cs/remote_segment: Respect batches_per_read in remote_segment_batch_consumer
    Same behavior as the bytes read budget, grabbing the number of batches produced
    so far instead.

--- src/v/cloud_storage/remote_segment.cc
+++ src/v/cloud_storage/remote_segment.cc
@@ -1422,10 +1422,16 @@ public:
         batch.header().header_crc = model::internal_header_only_crc(
           batch.header());
 
-        size_t sz = _seg_reader.produce(std::move(batch));
+        auto [sz, n_batches] = _seg_reader.produce(std::move(batch));
 
         auto now = model::timeout_clock::now();
         if (_config.over_budget || now > _deadline) {
+            co_return stop_parser::yes;
+        }
+
+        if (
+          _config.batches_per_read.has_value()
+          && n_batches >= _config.batches_per_read.value()) {
             co_return stop_parser::yes;
         }
 
@@ -1566,12 +1572,13 @@ remote_segment_batch_reader::init_parser(
     co_return parser;
 }
 
-size_t remote_segment_batch_reader::produce(model::record_batch batch) {
+std::pair<size_t, size_t>
+remote_segment_batch_reader::produce(model::record_batch batch) {
     ss::gate::holder h(_gate);
     vlog(_ctxlog.debug, "remote_segment_batch_reader::produce");
     _total_size += batch.size_bytes();
     _ringbuf.push_back(std::move(batch));
-    return _total_size;
+    return std::make_pair(_total_size, _ringbuf.size());
 }
 
 ss::future<> remote_segment_batch_reader::stop() {

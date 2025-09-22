cfaea0e5590fc1b129edcced9d73b5fd0b8decb4
Oren Leiman <oren.leiman@redpanda.com>
2025-09-21 21:09:59 -0700

    cs/remote_segment: remote_segment_batch_reader::bytes_consumed

--- src/v/cloud_storage/remote_segment.h
+++ src/v/cloud_storage/remote_segment.h
@@ -433,6 +433,8 @@ public:
 
     bool is_stopped() const { return _stopped; }
 
+    size_t bytes_consumed() const { return _config.bytes_consumed; }
+
 private:
     friend class single_record_consumer;
     ss::future<std::unique_ptr<storage::continuous_batch_parser>>

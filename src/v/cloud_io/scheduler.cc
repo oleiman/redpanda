28b686a76667f324b39aa2a711831d6ac3f5352a
Oren Leiman <oren.leiman@redpanda.com>
2026-05-20 15:42:07 -0700

    cloud_io: Wire up reservation_policy
    Replace the placeholder reservation case in scheduler::make_policy
    with construction of reservation_policy from the reservation_policy_config
    threaded through scheduler_config.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/v/cloud_io/scheduler.cc
+++ src/v/cloud_io/scheduler.cc
@@ -9,6 +9,7 @@
  */
 #include "cloud_io/scheduler.h"
 
+#include "cloud_io/reservation_policy.h"
 #include "cloud_io/scheduler_policy.h"
 
 #include <seastar/core/coroutine.hh>
@@ -48,7 +49,9 @@ scheduler::make_policy(size_t capacity, scheduler_config cfg) {
     case policy_type::passthrough:
         return std::make_unique<passthrough>(capacity);
     case policy_type::reservation:
-        throw std::runtime_error("reservation_policy: not implemented");
+        return std::make_unique<reservation_policy>(
+          capacity,
+          std::move(cfg.reservation).value_or(reservation_policy_config{}));
     }
     std::unreachable();
 }
